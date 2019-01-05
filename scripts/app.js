bcec061578d6c75f1da1795cb5bf29a26b46a90a
kikitaMac <kikitamoon@gmail.com>
2019-01-05 14:02:04 +0800

    HERE_Places_API
    HERE_Places_API

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,27 @@
-function requestSearchAPI() {
-    var url = "https://places.cit.api.here.com/places/v1/discover/search?q=cafe&in=52.530974%2C13.384944%3Br%3D120&app_id=oyNWTbKIov6XPMlrXxCH&app_code=_W_7qfgpOxthQ4tQQKkbLA"
+var url = "https://places.cit.api.here.com/places/v1/discover/search?q=cafe&in=52.530974%2C13.384944%3Br%3D120&app_id=oyNWTbKIov6XPMlrXxCH&app_code=_W_7qfgpOxthQ4tQQKkbLA"
 
+function postAction(li) {
+    var actionURL = "https://places.cit.api.here.com/places/v1/actions?app_id=oyNWTbKIov6XPMlrXxCH&app_code=_W_7qfgpOxthQ4tQQKkbLA&pretty"
+    //alert(JSON.parse(li.innerHTML).href)
+    $.ajax({
+        type: "POST",
+        url: actionURL,
+        contentType: "application/json",
+        data: JSON.stringify({
+            "actions": [{
+                "resource": JSON.parse(li.innerHTML).href,
+                "types": ["urn:nlp-actions:placeselected"],
+                "timestamp": "2019-01-12T14:34:10.763+0100"
+            }]
+        }),
+        success: function (data, status) {
+            alert("data: \n" + data + "\n status: " + status);
+        }
+    });
+}
+
+
+function requestSearchAPI() {
     var xmlhttp;
     if (window.XMLHttpRequest) {
         //  IE7+, Firefox, Chrome, Opera, Safari 
@@ -12,10 +33,18 @@ function requestSearchAPI() {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             document.getElementById("result1").innerHTML = "OK";
-            var jsonObj = JSON.parse(xmlhttp.responseText);  
+            var jsonObj = JSON.parse(xmlhttp.responseText);
+            var POIs = jsonObj.results.items
             // document.getElementById("result3").innerHTML = JSON.stringify(jsonObj.results.items[0].id) ;     
-            for (x in jsonObj.results.items) {
-                document.getElementById("result3").innerHTML += "Places ID : " + JSON.stringify(jsonObj.results.items[x].id) + "<br>";
+            for (x in POIs) {
+                // document.getElementById("result3").innerHTML += 
+                // "Places ID : " + JSON.stringify(jsonObj.results.items[x].id) + "<br>";
+                document.getElementById("list").innerHTML +=
+                    '<li onclick="postAction(this)" style="cursor:pointer">{"title": ' +
+                    JSON.stringify(POIs[x].title) +
+                    ',"id":' + JSON.stringify(POIs[x].id) +
+                    ',"href":' + JSON.stringify(POIs[x].href) +
+                    '}</li><br>';
             }
 
         }
